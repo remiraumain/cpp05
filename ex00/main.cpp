b1b418db0486ccbe58e486fcdd49f7a0685f1fa9
remiraumain <remiraumain@gmail.com>
2025-09-24 15:17:35 +0200

    ex00 done

---
+++ ex00/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rraumain <rraumain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/24 13:52:49 by rraumain          #+#    #+#             */
+/*   Updated: 2025/09/24 15:17:17 by rraumain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include <iostream>
+
+int	main()
+{
+	try
+	{
+		Bureaucrat	a("Bob", 100);
+		Bureaucrat	b("Jacky", 1);
+		// Bureaucrat	c("Michel", 150);
+		// Bureaucrat	d("Tom", 151);
+
+		std::cout << a << "\n";
+		std::cout << b << "\n";
+		// std::cout << c << "\n";
+		// std::cout << d << "\n";
+
+		a.increment();
+		b.increment();
+		// c.decrement();
+		// d.increment();
+
+		std::cout << a << "\n";
+		std::cout << b << "\n";
+		// std::cout << c << "\n";
+		// std::cout << d << "\n";
+	}
+	catch(const std::exception	&e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+	return (0);
+}
